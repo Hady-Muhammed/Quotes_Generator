8c0da3102174760223d57ee66806466efa01d6bb
Hady_Muhammed <hadyymohammed@gmail.com>
2022-08-21 21:45:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState , useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [advice,setAdvice] = useState('');
+
+    const GetQuote = async () => {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+    }
+
+    useEffect(() => {
+        GetQuote();
+    }, [])
+    
+    return (
+        <>
+        <q>{advice}</q>
+        <button onClick={() => GetQuote()}>GIVE ME ADVICE!</button>
+        </>
+    )
 }
 
 export default App;
